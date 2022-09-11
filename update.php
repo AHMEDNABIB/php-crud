993c48d30472a3147b8748828a0c0fc04db30c98
AHMED NABIB <nabibpallab@gmail.com>
2022-09-11 16:58:03 +0600

    data showed in udated from field

--- update.php
+++ update.php
@@ -55,25 +55,31 @@ $password =$row['password'];
         <form method='POST' >
             <div class="form-group mb-3">
                 <label>Name</label>
-                <input type="text" class="form-control" placeholder="Enter your name" name=' name' autocomplete='off'>
+                <input type="text" class="form-control" placeholder="Enter your name" name=' name' autocomplete='off'
+                value=<?php echo $name?>
+                >
 
             </div>
             <div class="form-group mb-3">
                 <label>Email</label>
-                <input type="email" class="form-control" placeholder="Enter your Email" name='email' autocomplete ='off'>
+                <input type="email" class="form-control" placeholder="Enter your Email" name='email' autocomplete ='off' value=<?php echo $email?>
+                
+                >
 
             </div>
             <div class="form-group mb-3">
                 <label>Mobile</label>
                 <input type="text" class="form-control" placeholder="Enter your Mobile No" name='mobile'
-                autocomplete='off'>
+                autocomplete='off'
+                value=<?php echo $mobile?>>
 
             </div>
 
             <div class="form-group mb-3">
                 <label>Password</label>
                 <input type="text" class="form-control" placeholder="Enter your Password" name='password'
-                autocomplete='off'>
+                autocomplete='off'
+                value=<?php echo $password?>>
 
             </div>
 
